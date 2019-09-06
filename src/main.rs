72c459a41ed944b593c0673952d196f41fc256ee
Colin Suckow <colin@suckow.dev>
2019-09-05 21:56:07 -0700

    Finished mem mapping

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 mod memory;
+mod cartridge;
 
 use memory::AddressSpace;
 
 fn main() {
     println!("Hello, world!");
-    let mut ram = memory::Ram::new(2048);
-    ram.poke(0, 15);
-    println!("Byte 0: {}", ram.peek(3000));
+    let mut memory_map = memory::MemMap::new();
+
+    //RAM
+    memory_map.add_mappable_mem(memory::MappableMem::new(
+        Box::new(memory::Ram::new(2048)),
+        100,
+    ));
+
+    println!("Memory map is {} bytes", memory_map.size());
+    memory_map.poke(0, 12);
+    memory_map.poke(103, 25);
+    println!(
+        "loc 0 = {:?}, loc 103 = {:?}",
+        memory_map.peek(0),
+        memory_map.peek(103)
+    );
+    cartridge::print_ines_header("BOMBMAN.NES");
 }
--- src/memory.rs
+++ src/memory.rs
@@ -29,39 +29,62 @@ impl AddressSpace for Ram {
   }
 }
 
-pub struct Mappable_Mem {
+pub struct MappableMem {
   data: Box<dyn AddressSpace>,
   offset: u16,
 }
 
-impl Mappable_Mem {
+impl MappableMem {
+  pub fn new(mem: Box<dyn AddressSpace>, mem_offset: u16) -> MappableMem {
+    MappableMem {
+      data: mem,
+      offset: mem_offset,
+    }
+  }
+
   pub fn map_ptr(&self, ptr: u16) -> Option<u16> {
     let data = &*self.data;
-    if ptr <= self.offset + data.size() as u16 && ptr >= self.offset {
+    if (ptr <= self.offset + data.size() as u16 && ptr >= self.offset)
+      && (ptr - self.offset) < data.size() as u16
+    {
       return Some(ptr - self.offset);
     }
     return None;
   }
 }
 
-pub struct Mem_Map {
-  mem: Vec<Mappable_Mem>,
+pub struct MemMap {
+  mem: Vec<MappableMem>,
 }
 
-impl Mem_Map {
-  pub fn new() -> Mem_Map {
-    Mem_Map { mem: Vec::new() }
+impl MemMap {
+  const MAX_ADDRESS: i32 = 65536;
+
+  pub fn new() -> MemMap {
+    MemMap { mem: Vec::new() }
   }
-  pub fn add_mappable_mem(&mut self, mem: Mappable_Mem) {
+  pub fn add_mappable_mem(&mut self, mem: MappableMem) {
     self.mem.push(mem);
   }
+
+  pub fn debug_print_memory(&self) {
+    for address in 0..2300 {
+      match self.peek(address as u16) {
+        Some(val) => print!("{} : {}, ", address, val),
+        None => print!("{} : NULL, ", address),
+      }
+      if address % 5 == 0 {
+        print!("\n");
+      }
+    }
+  }
 }
 
-impl AddressSpace for Mem_Map {
+impl AddressSpace for MemMap {
   fn peek(&self, ptr: u16) -> Option<u8> {
     for mem in &self.mem {
       match mem.map_ptr(ptr) {
-        Some(offsetPtr) => return mem.data.peek(ptr),
+        Some(offset_ptr) => return mem.data.peek(offset_ptr),
         None => {}
       }
     }
@@ -69,21 +92,21 @@ impl AddressSpace for Mem_Map {
   }
 
   fn poke(&mut self, ptr: u16, byte: u8) {
-    for mem in &self.mem {
+    for mem in &mut self.mem {
       match mem.map_ptr(ptr) {
-        Some(offsetPtr) => {
-          mem.data.poke(offsetPtr, byte);
-        },
+        Some(offset_ptr) => {
+          mem.data.poke(offset_ptr, byte);
+        }
         None => {}
       }
     }
   }
 
   fn size(&self) -> usize {
-    let totalSize = 0;
+    let mut total_size = 0;
     for mem in &self.mem {
-      totalSize += mem.data.size();
+      total_size += mem.data.size();
     }
-    return totalSize;
+    return total_size;
   }
 }
