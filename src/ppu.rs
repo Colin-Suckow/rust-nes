68b7eab88a049aaf958e6b828fe61bd2f1504cb0
Colin Suckow <csuckow99@gmail.com>
2020-11-03 22:22:05 -0800

    99% complete ppu palette support. A couple minor bugs left to work out

--- src/ppu.rs
+++ src/ppu.rs
@@ -5,6 +5,17 @@ use bit_field::BitField;
 pub const DISPLAY_WIDTH: usize = 256;
 pub const DISPLAY_HEIGHT: usize = 240;
 
+enum PaletteRam {
+    Background0,
+    Background1,
+    Background2,
+    Background3,
+    Sprite0,
+    Sprite1,
+    Sprite2,
+    Sprite3,
+}
+
 pub struct PPU {
     pub buffer: Vec<u32>,
     character_data: CharacterData,
@@ -24,7 +35,7 @@ pub struct PPU {
     ppuaddr_address: u16,
     nmi_fired: bool,
     oam_mem: Vec<u8>,
-    sprites_to_render: Vec<usize>,
+    palette_ram: Vec<u8>,
 }
 
 impl PPU {
@@ -48,7 +59,7 @@ impl PPU {
             ppuaddr_address: 0,
             nmi_fired: false,
             oam_mem: vec![0; 256],
-            sprites_to_render: Vec::<usize>::new(),
+            palette_ram: vec![0; 0x0020],
         }
     }
 
@@ -63,7 +74,8 @@ impl PPU {
                 false => TableHalf::Right,
             };
 
-            let offset = match self.PPUCTRL & 0x3 {
+            let nametable = self.PPUCTRL & 0x3;
+            let offset = match nametable {
                 0 => 0x2000,
                 1 => 0x2400,
                 2 => 0x2800,
@@ -87,26 +99,61 @@ impl PPU {
                 (self.y % 8) as i32,
             );
             //let color = if val > 0 { 0xFFFFFFFF } else { 0 };
-            let color: u32 = match val {
-                1 => 0xFF0000FF,
-                2 => 0x00FF00FF,
-                3 => 0x0000FFFF,
-                _ => 0x0,
-            };
+            let palette_segment = self.get_background_palette_segment(nametable as usize, self.x as usize, self.y as usize);
+            
+            let color = self.get_palette_color(&palette_segment, val as u16);
             let mx = self.x.clone() as usize;
             let my = self.y.clone() as usize;
             self.set_pixel(mx, my, color);
         }
     }
 
-    // fn update_sprites_to_render(&mut self) {
-    //     self.sprites_to_render.clear();
-    //     for (i, sprite) in self.oam_mem.chunks(4).enumerate() {
-    //         if sprite[0] >= y && sprite[0] < y+8 {
-    //             self.sprites_to_render.push(i)
-    //         }
-    //     }
-    // }
+    fn get_background_palette_segment(&self, nametable: usize, x: usize, y: usize) -> PaletteRam {
+        let offset = match nametable {
+            0 => 0x23C0,
+            1 => 0x27C0,
+            2 => 0x2BC0,
+            3 => 0x2FC0,
+            _ => 0,
+        };
+        let px = x / 32;
+        let py = y / 32;
+        let index = (py * 8) + px;
+        let pbyte = self.peek_vram((index + offset) as u16);
+        let val = match ((px * 32) + x, (py * 32) + y) {
+            (x, y) if x < 8 && y < 8 => pbyte & 0x3,          //topleft
+            (x, y) if x >= 8 && y < 8 => (pbyte >> 2) & 0x3,  //topright
+            (x, y) if x < 8 && y >= 8 => (pbyte >> 4) & 0x3,  //bottomleft
+            (x, y) if x >= 8 && y >= 8 => (pbyte >> 6) & 0x3, //tbottomright
+            _ => 0,
+        };
+        match val {
+            1 => PaletteRam::Background1,
+            2 => PaletteRam::Background2,
+            3 => PaletteRam::Background3,
+            _ => PaletteRam::Background0,
+        }
+    }
+
+    fn get_palette_color(&self, location: &PaletteRam, val: u16) -> u32 {
+        let color_code = {
+            if val == 0 {
+                self.peek_vram(0x3F00) //Universal background
+            } else {
+                match location {
+                    PaletteRam::Background0 => self.peek_vram((0x3F00 + val) as u16),
+                    PaletteRam::Background1 => self.peek_vram((0x3F04 + val) as u16),
+                    PaletteRam::Background2 => self.peek_vram((0x3F08 + val) as u16),
+                    PaletteRam::Background3 => self.peek_vram((0x3F0C + val) as u16),
+                    PaletteRam::Sprite0 => self.peek_vram((0x3F10 + val) as u16),
+                    PaletteRam::Sprite1 => self.peek_vram((0x3F14 + val) as u16),
+                    PaletteRam::Sprite2 => self.peek_vram((0x3F18 + val) as u16),
+                    PaletteRam::Sprite3 => self.peek_vram((0x3F1C + val) as u16),
+                }
+            }
+        };
+        PALETTE[color_code as usize]
+    }
 
     pub fn check_nmi(&mut self) -> bool {
         if self.PPUCTRL.get_bit(7) && self.y == 240 && !self.nmi_fired {
@@ -133,6 +180,12 @@ impl PPU {
             for sprite in self.oam_mem.clone().chunks(4) {
                 let tcol = sprite[1] & 0xF;
                 let trow = sprite[1] >> 4;
+                let segment = match sprite[2] & 0x3 {
+                    1 => PaletteRam::Sprite1,
+                    2 => PaletteRam::Sprite2,
+                    3 => PaletteRam::Sprite3,
+                    _ => PaletteRam::Sprite0,
+                };
                 self.draw_tile(
                     half,
                     sprite[3].clone() as i32,
@@ -141,6 +194,7 @@ impl PPU {
                     trow.clone() as i32,
                     sprite[2].get_bit(6),
                     sprite[2].get_bit(7),
+                    &segment
                 );
             }
 
@@ -157,6 +211,7 @@ impl PPU {
                 0x2400..=0x27FF => self.vram[(ptr - 0x2000) as usize] = byte,
                 0x2800..=0x2BFF => self.vram[(ptr - 0x2800) as usize] = byte,
                 0x2C00..=0x2FFF => self.vram[(ptr - 0x2400) as usize] = byte,
+                0x3F00..=0x3F1F => self.palette_ram[(ptr - 0x3F00) as usize] = byte,
                 _ => (),
             },
             MirrorMode::Horizontal => match ptr {
@@ -164,6 +219,7 @@ impl PPU {
                 0x2400..=0x27FF => self.vram[(ptr - 0x2000) as usize] = byte,
                 0x2800..=0x2BFF => self.vram[(ptr - 0x2400) as usize] = byte,
                 0x2C00..=0x2FFF => self.vram[(ptr - 0x2800) as usize] = byte,
+                0x3F00..=0x3F1F => self.palette_ram[(ptr - 0x3F00) as usize] = byte,
                 _ => (),
             },
         }
@@ -176,6 +232,7 @@ impl PPU {
                 0x2400..=0x27FF => self.vram[(ptr - 0x2000) as usize],
                 0x2800..=0x2BFF => self.vram[(ptr - 0x2800) as usize],
                 0x2C00..=0x2FFF => self.vram[(ptr - 0x2400) as usize],
+                0x3F00..=0x3F1F => self.palette_ram[(ptr - 0x3F00) as usize],
                 _ => 0,
             },
             MirrorMode::Horizontal => match ptr {
@@ -183,6 +240,7 @@ impl PPU {
                 0x2400..=0x27FF => self.vram[(ptr - 0x2000) as usize],
                 0x2800..=0x2BFF => self.vram[(ptr - 0x2400) as usize],
                 0x2C00..=0x2FFF => self.vram[(ptr - 0x2800) as usize],
+                0x3F00..=0x3F1F => self.palette_ram[(ptr - 0x3F00) as usize],
                 _ => 0,
             },
         }
@@ -224,6 +282,7 @@ impl PPU {
         tile_row: i32,
         mirror_h: bool,
         mirror_v: bool,
+        palette_segment: &PaletteRam,
     ) {
         for r in 0..8 {
             for c in 0..8 {
@@ -237,12 +296,9 @@ impl PPU {
                 }
                 let val =
                     self.get_background_pixel_value(half.clone(), tile_column, tile_row, mc, mr);
-                let color: u32 = match val {
-                    1 => 0xFF0000FF,
-                    2 => 0x00FF00FF,
-                    3 => 0x0000FFFF,
-                    _ => 0x0,
-                };
+                if val == 0 {continue;}
+
+                let color = self.get_palette_color(palette_segment, val as u16);
                 if ((((r + y) * DISPLAY_WIDTH as i32) + (c + x)) as usize) < 61440 {
                     self.set_pixel((c + x) as usize, (r + y) as usize, color);
                 };
@@ -340,6 +396,18 @@ impl AddressSpace for PPU {
         }
     }
 }
+
+//RGB palette values from emudev.de
+const PALETTE: [u32; 64] = [
+    0x7C7C7C, 0x0000FC, 0x0000BC, 0x4428BC, 0x940084, 0xA80020, 0xA81000, 0x881400, 0x503000,
+    0x007800, 0x006800, 0x005800, 0x004058, 0x000000, 0x000000, 0x000000, 0xBCBCBC, 0x0078F8,
+    0x0058F8, 0x6844FC, 0xD800CC, 0xE40058, 0xF83800, 0xE45C10, 0xAC7C00, 0x00B800, 0x00A800,
+    0x00A844, 0x008888, 0x000000, 0x000000, 0x000000, 0xF8F8F8, 0x3CBCFC, 0x6888FC, 0x9878F8,
+    0xF878F8, 0xF85898, 0xF87858, 0xFCA044, 0xF8B800, 0xB8F818, 0x58D854, 0x58F898, 0x00E8D8,
+    0x787878, 0x000000, 0x000000, 0xFCFCFC, 0xA4E4FC, 0xB8B8F8, 0xD8B8F8, 0xF8B8F8, 0xF8A4C0,
+    0xF0D0B0, 0xFCE0A8, 0xF8D878, 0xD8F878, 0xB8F8B8, 0xB8F8D8, 0x00FCFC, 0xF8D8F8, 0x000000,
+    0x000000,
+];
 
 #[derive(Clone, Copy)]
 enum TableHalf {
